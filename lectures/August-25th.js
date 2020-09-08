6e976889a04c956aca0bde5710c5765a706b450d
habahram <hamid.bahramian@gmail.com>
2020-09-07 20:23:09 -0400

    notes added.

---
+++ lectures/August-25th.js
@@ -0,0 +1,11 @@
+/*
+Install Nodejs: https://nodejs.org/en/download/
+Install Visual Studio Code: https://code.visualstudio.com/download
+Install Git: https://git-scm.com/downloads
+*/
+var x = 2;
+if(true) {
+    console.log(x);
+    let x = 90;
+}
+console.log(x);
